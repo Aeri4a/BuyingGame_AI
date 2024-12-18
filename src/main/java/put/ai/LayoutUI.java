794c1761f61e4809997f2b0d464185a03317b77d
Aeri4a <80831299+Aeri4a@users.noreply.github.com>
2024-12-18 20:54:53 +0100

    added background & styling

--- src/main/java/put/ai/LayoutUI.java
+++ src/main/java/put/ai/LayoutUI.java
@@ -1,17 +1,24 @@
 package put.ai;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class LayoutUI extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private final String FONT_NAME = "Futura";
+	private final Color buttonColor = new Color(43, 44, 39);
 	
 	// DROOLS
 	private KieServices ks;
@@ -21,16 +28,16 @@ public class LayoutUI extends JFrame {
 	// Main UI components
 	private CardLayout cardLayout;
 	private JPanel cardPanel;
-	private JPanel startPanel = new JPanel();
-	private JPanel contentPanel = new JPanel();
-	private JPanel endPanel = new JPanel();
+	private JPanel startPanel = new JPanelWithBackground("./assets/bg.png");
+	private JPanel contentPanel = new JPanelWithBackground("./assets/bg.png");
+	private JPanel endPanel = new JPanelWithBackground("./assets/bg.png");
 	private GridBagConstraints gbc = new GridBagConstraints();
+	private Border buttonBorder = BorderFactory.createLineBorder(Color.BLACK);
 	
 	// Drools interactive
 	private JLabel solutionText = new JLabel("");
 	private JPanel answerPanel = new JPanel();
 	private JLabel currentQuestion = new JLabel();
-	private ArrayList<Answer> currentAnswers;
 
 	public LayoutUI() {
 		super("BuyingGame");
@@ -42,29 +49,19 @@ public class LayoutUI extends JFrame {
 			ArrayList<Answer> questionAnswers
 	) {
 		this.currentQuestion.setText(questionContent);
-		this.currentAnswers = questionAnswers;
-		
-		// Update panel
-		this.contentPanel.remove(this.answerPanel);
-		this.answerPanel = new JPanel();
-		this.answerPanel.setLayout(new GridBagLayout());
-		this.contentPanel.revalidate();
+
+		this.answerPanel.removeAll();
 		this.answerPanel.revalidate();
-		this.contentPanel.add(this.answerPanel);
-	
 		
-//		this.solutionText.setFont(new Font(FONT_NAME, Font.BOLD, 20)); NO NEED
-		
-		// Centering
-		gbc.gridx = 0;
-		gbc.gridy = 1;
-		this.contentPanel.add(this.answerPanel, gbc);
-		
-		gbc.gridy = 1;
+		gbc.gridy = 0;
 		for (Answer answer : questionAnswers) {
 			JButton button = new JButton(answer.getContent());
-			button.setPreferredSize(new Dimension(400, 80));
+			button.setPreferredSize(new Dimension(400, 60));
+			button.setFocusPainted(false);
 			button.setFont(new Font(FONT_NAME, Font.PLAIN, 18));
+			button.setBorder(buttonBorder);
+			button.setBackground(this.buttonColor);
+			button.setForeground(Color.WHITE);
 			button.addActionListener(e -> {
 				kSession.insert(answer);
 	            kSession.fireAllRules();
@@ -82,19 +79,14 @@ public class LayoutUI extends JFrame {
 	
 	private void initialize() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(720, 480);
+		this.setSize(1000, 500);
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);
-		Container container = getContentPane();
-		container.setBackground(new Color(27,27,27));
 		this.setLayout(new BorderLayout());
 		
-//		this.startPanel.setBackground(new Color(27,27,27));
 		this.startPanel.setLayout(new GridBagLayout());
 		
-//		this.contentPanel.setBackground(new Color(27,27,27));
 		this.contentPanel.setLayout(new GridBagLayout());
-//		this.contentPanel.setLayout(new BorderLayout());
 				
 		this.endPanel.setLayout(new GridBagLayout());
 		JLabel solutionTextHeader = new JLabel("Game found!");
@@ -109,7 +101,7 @@ public class LayoutUI extends JFrame {
 		this.cardLayout = new CardLayout();
 		this.cardPanel = new JPanel(cardLayout);
 		this.cardPanel.add(startPanel, "StartPanel");
-		this.cardPanel.add(contentPanel, "ContentPanel"); // THIS
+		this.cardPanel.add(contentPanel, "ContentPanel");
 		this.cardPanel.add(endPanel, "EndPanel");
 		
 		this.add(cardPanel, BorderLayout.CENTER);
@@ -120,8 +112,19 @@ public class LayoutUI extends JFrame {
 	
 	private void displayStartView() {
 		JButton startButton = new JButton();
-		startButton.setText("Rozpocznij");
-		startButton.setPreferredSize(new Dimension(200, 100));
+		startButton.setText("I WANT TO BUY A GAME!");
+		startButton.setBorder(buttonBorder);
+		startButton.setFocusPainted(false);
+		startButton.setBackground(this.buttonColor);
+		startButton.setForeground(Color.WHITE);
+		startButton.setPreferredSize(new Dimension(400, 100));
+		startButton.setFont(new Font(FONT_NAME, Font.BOLD, 25));
+//		startButton.addFocusListener(new FocusAdapter() {
+//            @Override
+//            public void focusGained(FocusEvent e) {
+//            	e.getComponent().setBackground(Color.GREEN);
+//            }
+//        });
 		startButton.addActionListener(e -> {
 			try {
 	            // load up the knowledge base
@@ -129,9 +132,9 @@ public class LayoutUI extends JFrame {
 	    	    this.kContainer = ks.getKieClasspathContainer();
 	        	this.kSession = kContainer.newKieSession("ksession-rules");
 
+	        	displayContentView();
 	        	kSession.setGlobal("gui", this);
 	            kSession.fireAllRules();
-	            displayContentView();
 	        } catch (Throwable t) {
 	            t.printStackTrace();
 	        }
@@ -142,41 +145,38 @@ public class LayoutUI extends JFrame {
 	private void displayContentView() {
 		// Question
 		JPanel questionPanel = new JPanel();
-		this.currentQuestion.setFont(new Font(FONT_NAME, Font.PLAIN, 25));
+		questionPanel.setBackground(new Color(0, 0, 0, 90));
+		this.currentQuestion.setFont(new Font(FONT_NAME, Font.BOLD, 25));
+		this.currentQuestion.setForeground(Color.WHITE);
 		questionPanel.add(this.currentQuestion);
-				
-		// Answers
-		this.answerPanel.setLayout(new BoxLayout(answerPanel, BoxLayout.Y_AXIS));
-				
-		// Buttons -> OLD
-//		JPanel buttonPanel = new JPanel();
-//		JButton submitButton = new JButton("Submit");
-//		submitButton.addActionListener(e -> {
-//			JRadioButton selectedButton = currentOptions
-//					.stream()
-//					.filter(btn -> btn.isSelected())
-//					.findAny()
-//					.orElse(null);
-//			
-//			Answer answer = currentAnswers
-//					.stream()
-//					.filter(ans -> selectedButton.getText().equals(ans.getContent()))
-//					.findAny()
-//					.orElse(null);
-//			
-//			kSession.insert(answer);
-//            kSession.fireAllRules();
-//		});
-//		buttonPanel.add(submitButton);
 		
 		gbc.gridx = 0;
 		gbc.gridy = 0;
 		this.contentPanel.add(questionPanel, gbc);
+		
+		this.answerPanel.setLayout(new GridBagLayout());
 		gbc.gridy = 1;
 		this.contentPanel.add(this.answerPanel, gbc);
-//		gbc.gridy = 2;
-//		this.contentPanel.add(buttonPanel, gbc);
-			
+		
 		this.cardLayout.next(this.cardPanel);
 	}
+	
+	public class JPanelWithBackground extends JPanel {
+
+		  private Image backgroundImage;
+
+		  public JPanelWithBackground(String fileName) {
+			  try {
+				  backgroundImage = ImageIO.read(new File(fileName));	  
+			  } catch (IOException io) {
+				  
+			  }
+		    
+		  }
+
+		  public void paintComponent(Graphics g) {
+		    super.paintComponent(g);
+		    g.drawImage(backgroundImage, 0, 0, 1000, 500, this);
+		  }
+		}
 }
